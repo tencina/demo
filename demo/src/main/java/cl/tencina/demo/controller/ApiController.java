c27c0c1142a2d1e47ca43ace4265370b48a53abd
tencina <thenison.encina@gmail.com>
2021-08-05 09:57:27 -0400

    Update ApiController.java

--- demo/src/main/java/cl/tencina/demo/controller/ApiController.java
+++ demo/src/main/java/cl/tencina/demo/controller/ApiController.java
@@ -4,13 +4,9 @@ import cl.tencina.demo.exception.Response;
 import cl.tencina.demo.model.Resultado;
 import cl.tencina.demo.service.HistorialService;
 import cl.tencina.demo.service.UsuarioService;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
