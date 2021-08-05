9cc2a2b9791b411786e84dd4c985cbe2fbea3ff9
tencina <thenison.encina@gmail.com>
2021-08-05 09:26:54 -0400

    Add files via upload

---
+++ demo/src/main/java/cl/tencina/demo/exception/ExceptionHandlerController.java
@@ -0,0 +1,19 @@
+package cl.tencina.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(value= HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public Response requestHandlingNoHandlerFound() {
+        return new Response("Error", "Solicitud incorrecta");
+    }
+}
