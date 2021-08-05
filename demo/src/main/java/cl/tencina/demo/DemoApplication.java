2b07008073238a525dd0c11c319b5ed6c2a99683
tencina <thenison.encina@gmail.com>
2021-08-05 09:26:14 -0400

    Update DemoApplication.java

--- demo/src/main/java/cl/tencina/demo/DemoApplication.java
+++ demo/src/main/java/cl/tencina/demo/DemoApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableWebMvc
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+
+		DispatcherServlet dispatcherServlet = (DispatcherServlet)ctx.getBean("dispatcherServlet");
+		dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
 	}
 
 }
