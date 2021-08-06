83ecdf800dd157b828779c6bea4d7de5f40e8cc1
tencina <thenison.encina@gmail.com>
2021-08-06 11:54:06 -0400

    Update DemoApplication.java

--- demo/src/main/java/cl/tencina/demo/DemoApplication.java
+++ demo/src/main/java/cl/tencina/demo/DemoApplication.java
@@ -2,6 +2,9 @@ package cl.tencina.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
